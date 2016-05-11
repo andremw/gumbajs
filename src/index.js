3ec13219843a7afa4d3f415d24b7023951df6fb3
Andre Mazoni <mazoni.andre@gmail.com>
2016-05-10 23:22:31 -0300

    Removes unused file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const _createModel = require('./create-model.js');
+const createModel = require('./create-model.js');
 
 module.exports = mainConfig => {
     checkConfig(mainConfig);
 
     return {
         createModel: config => {
-            return _createModel(Object.assign(config, mainConfig));
+            return createModel(Object.assign(config, mainConfig));
         }
     };
 };
