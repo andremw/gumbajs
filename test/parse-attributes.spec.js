3b99828722d75c0db8c6e73af7cf096e275d1e57
Andre Mazoni <mazoni.andre@gmail.com>
2016-05-12 21:55:01 -0300

    Renames parse-model to parse-attributes

--- test/parse-attributes.spec.js
+++ test/parse-attributes.spec.js
@@ -1,56 +1,54 @@
 'use strict';
 
 const test = require('ava').test;
-const parseModel = require('../src/helper/parse-model');
+const parseAttribute = require('../src/helper/parse-attributes');
 
-// let simpleObj = null;
 let commonObj = null;
 
 test.beforeEach(() => {
     commonObj = {
         defaultValue: '{Boolean}false',
         name: './whatever',
-        xtype: ''
+        xtype: '',
+        allowBlank: 'true'
     };
-
-    // simpleObj = {
-    //     defaultValue: '{Boolean}false',
-    //     type: 'checkbox',
-    //     xtype: 'selection',
-    //     inputValue: 'yes',
-    //     name: './hideProductDropdown',
-    //     fieldLabel: 'Hide Product List',
-    //     fieldDescription: 'If checked, the product list will not be visible. The search will always be performed using the selected product.'
-    // };
 });
 
 test.only('defaultValue and name must be parsed correctly', t => {
-    const actual = parseModel(commonObj);
+    const actual = parseAttribute(commonObj);
     t.is(actual.defaultValue, 'false', 'defaultValue that contains {Boolean} must be parsed without it');
     t.is(actual.name, 'whatever', 'name that contains ./ must be parsed without it');
 });
 
 test.only('[xtype] textfield is parsed as string', t => {
     commonObj.xtype = 'textfield';
-    const actual = parseModel(commonObj);
+    const actual = parseAttribute(commonObj);
     t.is(actual.type, 'string', expectedMsg('string', actual.type));
 });
 
 test.only('[xtype] selection is parsed as string', t => {
     commonObj.xtype = 'selection';
-    const actual = parseModel(commonObj);
+    const actual = parseAttribute(commonObj);
     t.is(actual.type, 'string', expectedMsg('string', actual.type));
 });
 
 test.only('[xtype] colorfield is parsed as string', t => {
     commonObj.xtype = 'colorfield';
-    const actual = parseModel(commonObj);
+    const actual = parseAttribute(commonObj);
     t.is(actual.type, 'string', expectedMsg('string', actual.type));
 });
 
-test.todo('[xtype] multifield is parsed as list and with its key');
+test.only('allowBlank is parsed correctly', t => {
+    let actual = parseAttribute(commonObj);
+    t.is(actual.optional, true, expectedMsg(true, actual.optional));
+    commonObj.allowBlank = '{Boolean}false';
+    actual = parseAttribute(commonObj);
+    t.is(actual.optional, false, expectedMsg(false, actual.optional));
+});
 
-test.skip('parse model from simple object', t => {
+test.todo('[xtype] multifield is parsed as list');
+
+test.skip('parse attributes from simple object', t => {
     const simpleObj = {
         defaultValue: '{Boolean}false',
         type: 'checkbox',
@@ -66,7 +64,7 @@ test.skip('parse model from simple object', t => {
         defaultValue: 'false'
     };
 
-    const actual = parseModel(simpleObj);
+    const actual = parseAttribute(simpleObj);
 
     t.equal(actual, expected);
 });
