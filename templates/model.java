d2e1d0c79af5783f9aacbd85795c4757b1ce4503
Andre Mazoni <mazoni.andre@gmail.com>
2016-05-13 02:30:45 -0300

    Updates the model.java template

--- templates/model.java
+++ templates/model.java
@@ -7,10 +7,32 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 
 import org.apache.sling.api.resource.Resource;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Model(adaptables = Resource.class)
 public class {{modelName}} {
 
+    {{#modelAttrs}}
+    @Inject
+    {{#optional}}
+    @Optional
+    {{/optional}}
+    private {{#capitalize}}{{type}}{{/capitalize}} {{name}};
+
+    {{/modelAttrs}}
+    {{#modelAttrs}}
+    public {{#capitalize}}{{type}}{{/capitalize}} get{{#capitalize}}{{name}}{{/capitalize}}() {
+        return this.{{name}};
+    }
+
+    {{/modelAttrs}}
+    // configure the stylebuilder if needed
+
+    @PostConstruct
+    public void activate() {
+
+    }
 }
