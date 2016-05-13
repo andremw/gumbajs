fd0f76400976c88c328c537d6b44ee36f5aa97c4
Andre Mazoni <mazoni.andre@gmail.com>
2016-05-13 14:32:01 -0300

    Updates wcmuse template

--- templates/wcmuse.java
+++ templates/wcmuse.java
@@ -6,8 +6,20 @@ import com.tccc.marketingservices.utils.PrepareDictionaryHelper;
 
 public class {{controllerName}} extends WCMUse {
 
+    {{#models}}
+    private {{className}} {{modelName}};
+
+    {{/models}}
     @Override
     public void activate() throws Exception {
-
+        {{#models}}
+        this.{{modelName}} = getResource().adaptTo({{className}}.class);
+        {{/models}}
     }
+    {{#models}}
+    public {{className}} get{{classname}}() {
+        return this.{{modelName}};
+    }
+    
+    {{/models}}
 }
